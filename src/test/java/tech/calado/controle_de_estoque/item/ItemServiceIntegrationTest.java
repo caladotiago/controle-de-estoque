cbfd0976442325bcf0b97dcc3b4787a7d1ef227b
caladotiago <caladots@outlook.com>
2025-04-10 22:13:25 -0300

    chore: configure mockMvc

--- src/test/java/tech/calado/controle_de_estoque/item/ItemServiceIntegrationTest.java
+++ src/test/java/tech/calado/controle_de_estoque/item/ItemServiceIntegrationTest.java
@@ -3,18 +3,27 @@ package tech.calado.controle_de_estoque.item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import tech.calado.controle_de_estoque.support.PostgreSQLContainerTest;
 
 import java.util.List;
 
 import static java.util.UUID.randomUUID;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static tech.calado.controle_de_estoque.item.Grupo.LIMPEZA;
 import static tech.calado.controle_de_estoque.item.Unidade.UN;
 
+@AutoConfigureMockMvc
 public class ItemServiceIntegrationTest extends PostgreSQLContainerTest {
 
     @Autowired
-    ItemRepository itemRepository;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ItemRepository itemRepository;
 
     @BeforeEach
     void setup() {
@@ -22,7 +31,7 @@ public class ItemServiceIntegrationTest extends PostgreSQLContainerTest {
     }
 
     @Test
-    void should_retrieve_all_items() {
+    void should_retrieve_all_items() throws Exception {
         List<Item> items = List.of(
                 Item.of(
                         randomUUID(),
@@ -35,6 +44,10 @@ public class ItemServiceIntegrationTest extends PostgreSQLContainerTest {
                 )
         );
         itemRepository.saveAll(items);
+
+        this.mockMvc.perform(get("/v1/items"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1));
     }
 
 }
