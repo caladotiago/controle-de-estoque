2b325a701d679aaadabce35ee6d8d203b8ac30c0
caladotiago <caladots@outlook.com>
2025-04-13 11:33:42 -0300

    style: format code

--- src/main/java/tech/calado/controle_de_estoque/common/exception/ConflictException.java
+++ src/main/java/tech/calado/controle_de_estoque/common/exception/ConflictException.java
@@ -1,7 +1,9 @@
 package tech.calado.controle_de_estoque.common.exception;
 
 public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message, null, true, false);
-    }
+
+	public ConflictException(String message) {
+		super(message, null, true, false);
+	}
+
 }
--- src/main/java/tech/calado/controle_de_estoque/common/exception/ErrorHandler.java
+++ src/main/java/tech/calado/controle_de_estoque/common/exception/ErrorHandler.java
@@ -14,25 +14,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ErrorHandler {
 
-    private static final String CONTENT_TYPE = "Content-Type";
+	private static final String CONTENT_TYPE = "Content-Type";
 
-    private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
+	private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler({ Exception.class, Error.class })
-    public HttpEntity<Message> handlerException(Exception ex) {
-        Message message = new Message("internal server error");
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8);
-        return new ResponseEntity<>(message, responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ Exception.class, Error.class })
+	public HttpEntity<Message> handlerException(Exception ex) {
+		Message message = new Message("internal server error");
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8);
+		return new ResponseEntity<>(message, responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(ConflictException.class)
-    public HttpEntity<Object> handlerResourceConflictException(ConflictException ex) {
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8);
-        return new ResponseEntity<>(new Message(ex.getMessage()), responseHeaders, HttpStatus.CONFLICT);
-    }
+	@ResponseStatus(HttpStatus.CONFLICT)
+	@ExceptionHandler(ConflictException.class)
+	public HttpEntity<Object> handlerResourceConflictException(ConflictException ex) {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8);
+		return new ResponseEntity<>(new Message(ex.getMessage()), responseHeaders, HttpStatus.CONFLICT);
+	}
 
 }
